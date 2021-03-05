ec82a3241d6f99d38ebd1e3876831eef09875299
vramirez-dacartec <vramirez@dacartec.com>
2021-03-05 09:08:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 3d-effect
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/3d-effect)
---
+++ index.js
@@ -0,0 +1,6 @@
+// Import stylesheets
+import "./style.css";
+
+// Write Javascript code!
+const container = document.querySelector(".container");
+container.innerHTML = `<h1>JS Starter</h1>`;
